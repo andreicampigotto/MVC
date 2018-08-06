a23ee293d2a1db18d576eb01d2af0398f5263c1f
andreicampigotto <andreicampigotto@gmail.com>
2018-08-06 16:28:37 -0300

    bla
    blabla

--- ExemploMVC02/ExemploMVC02/Controllers/RecrutadoraController.cs
+++ ExemploMVC02/ExemploMVC02/Controllers/RecrutadoraController.cs
@@ -15,15 +15,25 @@ namespace ExemploMVC02.Controllers
         {
             List<Recrutadora> recrutadoras = new RecrutadoraRepositorio().ObterTodos();
             ViewBag.Recrutadoras = recrutadoras;
+            ViewBag.TituloPagina = "Recrutadoras";
             return View();
         }
         public ActionResult Cadastro()
         {
+            ViewBag.TituloPagina = "Recrutadoras - Cadastro";        
             return View();
         }
         public ActionResult Editar()
         {
+            Recrutadora recrutadora = new RecrutadoraRepositorio().ObterPeloID(id);
+            ViewBag.Recrutadora = recrutadora;
             return View();
         }
+        public ActionResult Excluir()
+        {
+
+            return View();
+        }
+
     }
 }
